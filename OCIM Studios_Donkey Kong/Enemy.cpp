e21197b258b2c6beb6593fa8ace3afcce03eb73c
FireAlfa <firealfa.gaming@gmail.com>
2020-06-14 08:39:08 +0200

    Revert "Enemies Collisions"
    This reverts commit dfc08ff0263cecaaf8ab305d0e67e0c99c8cd4d2.

--- OCIM Studios_Donkey Kong/Enemy.cpp
+++ OCIM Studios_Donkey Kong/Enemy.cpp
@@ -1,4 +1,5 @@
 #include "Enemy.h"
+
 #include "Application.h"
 #include "ModuleCollisions.h"
 #include "ModuleParticles.h"
@@ -8,9 +9,7 @@
 //Constructor sets the initial position of enemy
 Enemy::Enemy(int x, int y) : position(x, y)
 {
-	enemState = MOVING;
 	spawnPos = position;
-	srand(time(NULL));
 }
 
 //Destroys enemy collider
@@ -46,138 +45,17 @@ void Enemy::Draw()
 //Control the collisions between the enemy and other colliders
 void Enemy::OnCollision(Collider* c1, Collider* c2)
 {
-	switch (enemType)
+	if (c1 == collider && c2->type == Collider::Type::TOP_STAIR) {
+		enemyCanFall = true;
+	}
+
+	if (c1 == collider && c2->type == Collider::Type::RIGHTWALL)
 	{
-	case BARREL:
-		//Ramp right collision
-		if (enemState != FALLING) {
-			enemState = MOVING;
-			if (c1 == collider && c2->type == Collider::Type::RAMP_RIGHT)
-			{
-				rampRight = true;
-				aux = c2->GetRect();
-				lastCollider = Collider::Type::RAMP_RIGHT;
-			}
-			//Ramp left collision
-			if (c1 == collider && c2->type == Collider::Type::RAMP_LEFT)
-			{
-				rampLeft = true;
-				aux = c2->GetRect();
-				lastCollider = Collider::Type::RAMP_LEFT;
-			}
-		}
-		//Collision with top Stair
-		if (c1 == collider && c2->type == Collider::Type::TOP_STAIR)
-		{
-			randomValue = rand() % 3;
-			if (randomValue == 0)
-			{
-				enemState = FALLING;
-			}
-			else {
-				enemState = MOVING;
-			}
-		}
-		//Map Border Bounce
-		if (enemState == MOVING && c1 == collider && c2->type == Collider::Type::RIGHTWALL || Collider::Type::LEFTWALL)
-		{
-			randomValue = rand() % 2;
-			if (randomValue == 0)
-			{
-				Enemy::facingDirection *= -1;
-			}
-		}
-		break;
-	case BLUEBARREL:
-		//Ramp right collision
-		if (enemState != FALLING) {
-			enemState = MOVING;
-			if (c1 == collider && c2->type == Collider::Type::RAMP_RIGHT)
-			{
-				rampRight = true;
-				aux = c2->GetRect();
-				lastCollider = Collider::Type::RAMP_RIGHT;
-			}
-			//Ramp left collision
-			if (c1 == collider && c2->type == Collider::Type::RAMP_LEFT)
-			{
-				rampLeft = true;
-				aux = c2->GetRect();
-				lastCollider = Collider::Type::RAMP_LEFT;
-			}
-		}
-		//Collision with top Stair
-		if (c1 == collider && c2->type == Collider::Type::TOP_STAIR)
-		{
-			randomValue = rand() % 3;
-			if (randomValue == 0)
-			{
-				enemState = FALLING;
-			}
-			else {
-				enemState = MOVING;
-			}
-		}
-		//Map Border Bounce
-		if (enemState == MOVING && c1 == collider && c2->type == Collider::Type::RIGHTWALL || Collider::Type::LEFTWALL)
-		{
-			randomValue = rand() % 2;
-			if (randomValue == 0)
-			{
-				Enemy::facingDirection *= -1;
-			}
-		}
-		break;
-	case FIRE:
-		//Collision with top Stair
-		if (c1 == collider && c2->type == Collider::Type::TOP_STAIR)
-		{
-			randomValue = rand() % 2;
-			if (randomValue == 0)
-			{
-				enemState = FALLING;
-			}
-			else {
-				enemState = MOVING;
-			}
-		}
-		//End  of platform Collision
-		if (enemState == MOVING && c1 == collider && c2->type == Collider::Type::GRAVITYWALLS || Collider::Type::RIGHTWALL || Collider::Type::LEFTWALL) 
-		{
-			Enemy::facingDirection *= -1;
-		}
-		//Stair Collision
-		if (c1 == collider && c2->type == Collider::Type::STAIR)
-		{
-			randomValue = rand() % 2;
-			if (randomValue == 0)
-			{
-				enemState = CLIMBING;
-			}
-			else {
-				enemState = MOVING;
-			}
-		}
-		//Ramp right collision
-		if (c1 == collider && c2->type == Collider::Type::RAMP_RIGHT)
-		{
-			rampRight = true;
-			aux = c2->GetRect();
-			lastCollider = Collider::Type::RAMP_RIGHT;
-		}
-		//Ramp left collision
-		if (c1 == collider && c2->type == Collider::Type::RAMP_LEFT)
-		{
-			rampLeft = true;
-			aux = c2->GetRect();
-			lastCollider = Collider::Type::RAMP_LEFT;
-		}
-		
-		break;
-	case CEMENT:
-		break;
-	default:
-		break;
+		enemyHitRight = true;
+	}
+	if (c1 == collider && c2->type == Collider::Type::LEFTWALL)
+	{
+		enemyHitLeft = true;
 	}
 }
 
